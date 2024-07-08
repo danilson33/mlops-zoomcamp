f521969231002abf84a2d76eeb2e1caf71b039c0
Serhii Baranovych <baranovychsergij@gmail.com>
2024-07-08 22:11:48 +0200

    Add files

---
+++ 06-best-practices/batch_flow.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys 
+import os
+import pandas as pd
+import pickle
+
+
+def read_data(filename, categorical):
+    s3_endpoint_url = os.getenv('S3_ENDPOINT_URL')
+    
+    if s3_endpoint_url:
+        options = {
+            'client_kwargs': {
+                'endpoint_url': s3_endpoint_url
+            }
+        }
+        df = pd.read_parquet(filename, storage_options=options)
+    else:
+        df = pd.read_parquet(filename)
+    return df
+
+def prepare_data(df, categorical):
+    df['duration'] = df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']
+    df['duration'] = df['duration'].dt.total_seconds()/60
+    
+    df =  df[(df['duration'] >= 1) & (df['duration'] <=60)].copy()
+    
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    return df
+
+def save_data(df, output_file, options):
+    df.to_parquet(
+        output_file,
+        engine='pyarrow',
+        compression=None,
+        index=False,
+        storage_options=options
+    )
+
+def main(year, month):
+    input_file = f's3://nyc-duration/integration_test/yellow_tripdata_{year:04d}-{month:02d}.parquet'
+    output_file = f's3://nyc-duration/predicted_data/taxi_type=yellow_year={year:04d}_month={month:02d}.parquet'
+    
+    with open('model.bin', 'rb') as f_in:
+        dv, lr = pickle.load(f_in)
+    
+    categorical = ['PULocationID', 'DOLocationID']
+    
+    
+    df = read_data(input_file, categorical)
+    df = prepare_data(df, categorical)
+    
+    df['ride_id'] = f'{year:04d}/{month:02d}_' + df.index.astype('str')
+    
+    dicts = df[categorical].to_dict(orient='records')
+    X_val = dv.transform(dicts)
+    y_pred = lr.predict(X_val)
+    
+    print('predicted mean duration:', y_pred.mean())
+    
+    df_result = pd.DataFrame()
+    df_result['ride_id'] = df['ride_id']
+    df_result['predicted_duration'] = y_pred 
+    
+    options = {
+        'client_kwargs': {
+            'endpoint_url': os.getenv('S3_ENDPOINT_URL', 'http://localhost:4566')
+        }
+    }
+    
+    save_data(df_result, output_file, options)
+    
+if __name__ == '__main__':
+    year = int(sys.argv[1])
+    month = int(sys.argv[2])
+    main(year, month)
---
+++ 06-best-practices/tests/test_batch.py
@@ -0,0 +1,46 @@
+import pytest
+import pandas as pd
+from datetime import datetime
+
+def dt(hour, minute, second=0):
+    return datetime(2023, 1, 1, hour, minute, second)
+
+def prepare_data(df, categorical):
+    df['duration'] = df['tpep_dropoff_datetime'] - df['tpep_pickup_datetime']
+    df['duration'] = df['duration'].dt.total_seconds() / 60
+
+    df = df[(df['duration'] >= 1) & (df['duration'] <= 60)].copy()
+    df[categorical] = df[categorical].fillna(-1).astype('int').astype('str')
+    
+    return df
+
+
+def test_prepare():
+    test_data = [
+        (None, None, dt(1, 1), dt(1, 10)),
+        (1, 1, dt(1, 2), dt(1, 10)),
+        (1, None, dt(1, 2, 0), dt(1, 2, 59)),
+        (3, 4, dt(1, 2, 0), dt(2, 2, 1)),
+    ]
+
+    columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime']
+    test_df = pd.DataFrame(test_data, columns=columns)
+
+    expected_data = [
+        (-1, -1, dt(1, 1), dt(1, 10), 9.0),
+        (1, 1, dt(1, 2), dt(1, 10), 8.0),
+    ]
+
+    expected_columns = ['PULocationID', 'DOLocationID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime', 'duration']
+    expected_df = pd.DataFrame(expected_data, columns=expected_columns)
+    expected_df['PULocationID'] = expected_df['PULocationID'].astype(str)
+    expected_df['DOLocationID'] = expected_df['DOLocationID'].astype(str)
+
+    categorical = ['PULocationID', 'DOLocationID']
+    result_df = prepare_data(test_df, categorical)
+
+    pd.testing.assert_frame_equal(result_df, expected_df, check_dtype=True)
+
+
+if __name__ == "__main__":
+    pytest.main([__file__])
